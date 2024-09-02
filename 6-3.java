49a8bebe4e7318a1c3ee41265abf73113b366be7
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2024-09-02 23:44:51 +0900

    Update 6-3.java

--- 6-3.java
+++ 6-3.java
@@ -1,16 +1,31 @@
-String name;
-int ban;
-int no;
-int kor;
-int eng;
-int math;
-
-getTotal(kor, eng, math) {
-  int total = kor + eng + math;
-  return total;
-}
-
-getTotal(kor, eng, math) {
-  float average = (getTotal()/3.0);
-  return average;
-}
+class Student{
+	String name;
+    int ban;
+    int no;
+    int kor;
+    int eng;
+    int math;
+    
+    Student(){} // 기본 생성자
+    
+    Student(String name, int ban, int no, int kor, int eng, int math){
+    	this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+     }
+     
+     int getTotal(){
+     		return kor+eng+math;
+     }
+     
+     float getAverage(){
+     		return (int)(getTotal() / 3f * 10 + 0.5f) / 10f;
+     }
+     
+     public String info(){
+ 		return name+","+ban+","+no+","+kor+","+eng+","+math+","+getTotal()
+        +","+getAverage()
+     }
