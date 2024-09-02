9ccbd584865a4771f7277b835084730e51640fbe
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2024-09-02 23:47:03 +0900

    Update 6-7.java

--- 6-7.java
+++ 6-7.java
@@ -1,21 +1,6 @@
-class Data { int x;}
-
-
-public class Main {
-
-    public static void main(String[] args) {
-        Data d = new Data();
-
-        d.x = 10;
-        System.out.println("main() : x = " + d.x);
-
-        change(d);
-        System.out.println("After Change(d)");
-        System.out.println("main() : x = " + d.x);
-
-    }
-    static void change(Data d) {
-        d.x= 1000;
-        System.out.println("change() : x = " + d.x);
-    }
-}
+staticintweapon=6;
+staticintarmor=0;
+static void weaponUp) f
+weapon++;
+static void armorUp o) i
+armort+;
