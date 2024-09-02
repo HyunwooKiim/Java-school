37fafed6410a335a88bbd026e488773441f96e47
김현우 <162122452+HyunwooKiim@users.noreply.github.com>
2024-09-02 23:18:06 +0900

    Update 6-1.java
    저능한짓헀어요

--- 6-1.java
+++ 6-1.java
@@ -1,21 +1,6 @@
-class Tv {
-    String color;
-    boolean power;
-    int channel;
-
-    void power() {power = !power; }
-    void channelUp(){++channel; }
-    void channelDown(){--channel; }
-}
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        Tv t = new Tv();
-        t.channel=7;
-        t.channelDown();
-        System.out.println("현재 채널은 "+ t.channel + "입니다.");
-
-    }
-}
+String name;
+int ban;
+int no;
+int kor;
+int eng;
+int math;
